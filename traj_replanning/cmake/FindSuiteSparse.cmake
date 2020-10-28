146b6cc0e26128e8f9035d6a8708dd7e6d9a6ec3
Dinesh Thakur <tdinesh@seas.upenn.edu>
2020-10-28 15:01:59 -0400

    add aarch64 to search paths in findSuiteSparse

--- traj_replanning/cmake/FindSuiteSparse.cmake
+++ traj_replanning/cmake/FindSuiteSparse.cmake
@@ -9,7 +9,7 @@ FIND_PATH(CHOLMOD_INCLUDE_DIR NAMES cholmod.h amd.h camd.h
     NO_DEFAULT_PATH
   )
 
-SET(SEARCH_PATHS /usr/local/lib;/usr/lib;/opt/local/lib;/sw/lib;/usr/lib/x86_64-linux-gnu;/usr/lib/arm-linux-gnueabihf)
+SET(SEARCH_PATHS /usr/local/lib;/usr/lib;/opt/local/lib;/sw/lib;/usr/lib/x86_64-linux-gnu;/usr/lib/arm-linux-gnueabihf;/usr/lib/aarch64-linux-gnu)
 
 
 
